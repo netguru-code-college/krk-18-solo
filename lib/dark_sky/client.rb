888c02d40e61ab8358e5202ace6b083c418e5c5f
Norbert Nytko <norbert.nytko.extern@solarisbank.de>
2018-07-21 16:40:31 +0200

    Fix specs

--- lib/dark_sky/client.rb
+++ lib/dark_sky/client.rb
@@ -5,14 +5,14 @@ module DarkSky
     # metoda zwraca temperature w formie INT
     # TODO przeniesc key to zmiennych srodowiskowych / config
     KEY = "6a9cdcf480ea84b91028a6e52634c8da"
-    
+
     def get_temperature(lat, lang)
       response = Faraday.get "https://api.darksky.net/forecast/#{KEY}/#{lat},#{lang}"
       convert_farenheit_to_celsius (JSON.parse(response.body)['currently']['temperature'])
     end
 
     def convert_farenheit_to_celsius(temperature)
-      ((temperature - 32) / 1.8000)
+      ((temperature - 32) / 1.8000).floor
     end
   end
 end
