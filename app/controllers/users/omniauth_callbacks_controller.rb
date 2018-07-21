3d1a79cab689c0467955989c5b6b26b066ca60a0
kchodorek <chodorek.kamil@gmail.com>
2018-07-21 14:59:52 +0200

    Added conversion from Farenheit to Celsius

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    binding.pry
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
--- lib/dark_sky/client.rb
+++ lib/dark_sky/client.rb
@@ -2,13 +2,17 @@ require 'json'
 
 module DarkSky
   class Client
-    #  metoda zwraca temperature w formie INT
+    # metoda zwraca temperature w formie INT
     # TODO przeniesc key to zmiennych srodowiskowych / config
     KEY = "6a9cdcf480ea84b91028a6e52634c8da"
     
     def get_temperature(lat, lang)
       response = Faraday.get "https://api.darksky.net/forecast/#{KEY}/#{lat},#{lang}"
-      JSON.parse(response.body)['currently']['temperature']
+      convert_to_celsius(JSON.parse(response.body)['currently']['temperature'])
+    end
+
+    def convert_to_celsius(temperature)
+      ((temperature - 32) / 1.8000)
     end
   end
 end
