ba0d4e4cea36e94debce2091f64bb197945db259
kchodorek <chodorek.kamil@gmail.com>
2018-07-22 14:22:47 +0200

    Implemented Send notification via SMS

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
   def create
     SMSService.new.call(
-      body: "Piję #{} w #{} przy temperaturze #{}",
-      to: mobile
+      body: "Piję #{params[:notify][:alcohol]} w #{params[:notify][:city]} przy temperaturze #{params[:notify][:temperature]} stopni",
+      to: params[:notify][:phone_number]
     )
   end
 end
