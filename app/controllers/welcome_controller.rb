3c52eb19028cd0e260fe596e98cfffa2e4bea7af
Kamil Chodorek <kamil.chodorek@gmail.com>
2018-07-22 09:59:51 +0200

    added private method in controller for get city temperature (#18)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  private 
+
+  def get_city_with_weather_in_city_service
+    WeatherInCity.new.call(params[:city_name])
+  end
 end
