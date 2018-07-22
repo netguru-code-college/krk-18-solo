5e7ce7fcfacc4824e62a66de191ffd64028416bf
Kamil Chodorek <kamil.chodorek@gmail.com>
2018-07-22 10:52:39 +0200

    Form for search city (#21)
    * implemented form for search city
    
    * search city form connected with class

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,24 @@
 class WelcomeController < ApplicationController
   def index
+    # binding.pry
+    # @city_name = params['city']['name']
+  end
+
+  def get_city
+    @temperature = get_city_with_weather_in_city_service
+    # binding.pry
+    # redirect_to 'welcome/show', city_name: params[:city][:name], temperature: @temperature
+    redirect_to action: 'show', city_name: params[:city][:name], temperature: @temperature
+  end
+
+  def show
+    @temperature = params[:temperature]
+    @city = params[:city_name]
   end
 
   private 
 
   def get_city_with_weather_in_city_service
-    WeatherInCity.new.call(params[:city_name])
+    WeatherInCity.new.call(params[:city][:name])
   end
 end
